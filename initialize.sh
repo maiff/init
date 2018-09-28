edb307d3d410c747cdb03f077d52255b03df0725
Xue Qianming <xueqianming200@gmail.com>
2018-09-28 16:48:49 +0800

    装机脚本添加git配置

---
+++ initialize.sh
@@ -0,0 +1,15 @@
+sudo apt-get install zsh
+sudo apt-get install gvim
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo apt-get install pip
+pip3 install ipython3
+rsync -av ./ ~/
+sudo apt install build-essential cmake python3-dev
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --clang-completer
+cd -
+wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+chmod +x Anaconda3-5.2.0-Linux-x86_64.sh
+./Anaconda3-5.2.0-Linux-x86_64.sh
+git config --global user.email "xueqianming200@gmail.com"
+git config --global user.name "Xue Qianming"
