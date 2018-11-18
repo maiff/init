db12289fa1d7368737cdc597e3524846718607d8
Xue Qianming <xueqianming200@gmail.com>
2018-11-18 12:27:32 +0800

    initialization updated

--- non-root-initialize.sh
+++ non-root-initialize.sh
@@ -1,6 +1,3 @@
-mkdir ~/usr
-cd ~
-
 # brew installation
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
@@ -8,38 +5,29 @@ test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home
 test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
 echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
 
-# zsh installation
-#sudo apt-get install zsh
-
 ## vim installation
-#git clone https://github.com/vim/vim.git
-#cd vim/src
-#make distclean
-#LDFLAGS=-L$HOME/usr/lib ./configure --prefix=$HOME/usr --
-#sudo apt-get install gvim
 brew install vim
+
+# oh-my-zsh installation
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# configuration files importation
+rsync -av ./ ~/
 
 # tmux configuration
 cd
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
-
-# oh-my-zsh installation
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# pip installation
-sudo apt-get install python-pip
+cp ~/Home/.tmux.conf.local .
 
 # ipython installation
-pip3 install ipython3
+pip3 install ipython
 
-# configuration files importation
-rsync -av ./ ~/
+source ~/.zshrc
 
-#sudo apt install build-essential cmake python3-dev
 # youcompleteme compilation
-brew install cmake python3-dev
+brew install cmake
+#brew install python3-dev
 cd ~/.vim/bundle/YouCompleteMe
 python3 install.py --clang-completer
 cd -
